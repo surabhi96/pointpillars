898e8182726240d350c38a397c55e3c9540f31ea
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-05-11 19:25:56 +0000

    first working commit

---
+++ SparseConvNet/sparseconvnet/metadata.py
@@ -0,0 +1,19 @@
+# Copyright 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+Store Metadata relating to which spatial locations are active at each scale.
+Convolutions, submanifold convolutions and 'convolution reversing' deconvolutions
+all coexist within the same MetaData object as long as each spatial size
+only occurs once.
+"""
+import sys
+sys.path.append("/usr/lib/x86_64-linux-gnu")
+
+import sparseconvnet.SCN
+
+def Metadata(dim):
+    return getattr(sparseconvnet.SCN, 'Metadata_%d'%dim)()
---
+++ second.pytorch/second/core/non_max_suppression/__init__.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("/home/sverma/second.pytorch/second/core/")
+from non_max_suppression.nms_cpu import nms_jit, soft_nms_jit
+from non_max_suppression.nms_gpu import (nms_gpu, rotate_iou_gpu,
+                                                       rotate_nms_gpu)
